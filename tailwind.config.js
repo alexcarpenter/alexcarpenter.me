805d9e93ae841071df7c52c5bfda1afc776f02bc
Alex Carpenter <im.alexcarpenter@gmail.com>
2020-11-30 18:44:34 -0500

    Update purge settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
