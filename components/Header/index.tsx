174c5e76d031c51e3668613df41bc8f1b01bb8d9
Alex Carpenter <im.alexcarpenter@gmail.com>
2020-12-12 16:15:15 -0500

    Remove object fit

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,21 +1,22 @@
 function Header({ children }) {
-  return (
-    <header className='space-y-3'>
-      {children}
-    </header>
-  );
+  return <header className='space-y-3'>{children}</header>;
 }
 
 function Title({ children, decorate = true }) {
   return (
-    <h1 className='text-4xl font-bold tracking-tight'>
-      {children}{decorate && (<span aria-hidden='true' className='text-gray-400 ml-2'>¬</span>)}
+    <h1 className='text-3xl sm:text-4xl font-bold tracking-tight'>
+      {children}
+      {decorate && (
+        <span aria-hidden='true' className='text-gray-400 ml-2'>
+          ¬
+        </span>
+      )}
     </h1>
   );
 }
 
 function Description({ children }) {
-  return <p className='text-2xl text-gray-600'>{children}</p>;
+  return <p className='text-xl sm:text-2xl text-gray-600'>{children}</p>;
 }
 
 Header.Title = Title;
