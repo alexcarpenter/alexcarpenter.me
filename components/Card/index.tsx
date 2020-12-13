4bb41c74dbdc1f8f65ca9beb7bb0269a19f6dce1
Alex Carpenter <im.alexcarpenter@gmail.com>
2020-12-13 13:55:54 -0500

    Green highlight

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,29 +1,21 @@
 function Card({ children, highlight = false }) {
-  return (
-    <div className={`relative flex flex-col p-4 rounded-md bg-white border shadow-sm ${highlight ? 'border-blue' : 'border-gray-200'}`}>
-      {children}
-    </div>
-  )
+  return <div className={`relative flex flex-col p-4 rounded-md bg-white border shadow-sm ${highlight ? "border-green" : "border-gray-200"}`}>{children}</div>;
 }
 
 function Eyebrow({ children }) {
-  return <h2 className="text-gray-600 uppercase tracking-wide">{children}</h2>
+  return <h2 className='text-gray-600 uppercase tracking-wide'>{children}</h2>;
 }
 
 function Title({ children }) {
-  return (
-    <h2 className='text-xl font-bold'>
-      {children}
-    </h2>
-  );
+  return <h2 className='text-xl font-bold'>{children}</h2>;
 }
 
 function Stat({ children }) {
-  return <p className="text-4xl font-bold">{children}</p>
+  return <p className='text-4xl font-bold'>{children}</p>;
 }
 
 function Description({ children, italic = false }) {
-  return <p className={`mt-2 text-gray-600${italic ? ' italic' : ''}`}>{children}</p>;
+  return <p className={`mt-2 text-gray-600${italic ? " italic" : ""}`}>{children}</p>;
 }
 
 Card.Eyebrow = Eyebrow;
