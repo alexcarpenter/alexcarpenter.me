8132cee918ce7e87fa05120a3b095b69a6c1643c
Alex Carpenter <im.alexcarpenter@gmail.com>
2020-12-14 20:27:32 -0500

    Fix youtube metrics

--- components/Metrics/Youtube.tsx
+++ components/Metrics/Youtube.tsx
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
 import Card from "@/components/Card";
-import Grid from "@/components/Grid";
+import Stack from "@/components/Stack";
 
 export default function Youtube() {
   const { data } = useSWR("/api/youtube", fetcher);
@@ -9,15 +9,22 @@ export default function Youtube() {
   const viewCount = data?.viewCount;
 
   return (
-    <Grid>
-      <Card>
-        <Card.Eyebrow>Total subscribers</Card.Eyebrow>
-        <Card.Stat>{subscriberCount ? subscriberCount : "-"}</Card.Stat>
-      </Card>
-      <Card>
-        <Card.Eyebrow>Total views</Card.Eyebrow>
-        <Card.Stat>{viewCount ? `${Math.round(viewCount / 1000)}K+` : "-"}</Card.Stat>
-      </Card>
-    </Grid>
+    <Stack grid>
+      <Stack.Item>
+        <Card>
+          <Card.Eyebrow>Total subscribers</Card.Eyebrow>
+          <Card.Stat>{subscriberCount ? subscriberCount : "-"}</Card.Stat>
+        </Card>
+      </Stack.Item>
+
+      <Stack.Item>
+        <Card>
+          <Card.Eyebrow>Total views</Card.Eyebrow>
+          <Card.Stat>
+            {viewCount ? `${Math.round(viewCount / 1000)}K+` : "-"}
+          </Card.Stat>
+        </Card>
+      </Stack.Item>
+    </Stack>
   );
 }
