9926016744027a33f2939ffee37f48055f2eaad6
Alex Carpenter <im.alexcarpenter@gmail.com>
2020-12-17 09:07:47 -0500

    Add next/previous links

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -1,15 +1,21 @@
+import Link from "next/link";
+import Card from "@/components/Card";
 import Header from "@/components/Header";
 import Page from "@/components/Page";
-import YoutubeVideo from "@/components/YoutubeVideo";
+import Stack from "@/components/Stack";
 import YoutubeSubscribe from "@/components/YoutubeSubscribe";
+import YoutubeVideo from "@/components/YoutubeVideo";
 
-export default function Post({ frontMatter, content }) {
+export default function Post(props) {
+  const { frontMatter, content, next, previous } = props;
   return (
     <Page {...frontMatter}>
       <article>
         <Header>
           <Header.Title decorate={false}>{frontMatter.title}</Header.Title>
-          {frontMatter.description && <Header.Description>{frontMatter.description}</Header.Description>}
+          {frontMatter.description && (
+            <Header.Description>{frontMatter.description}</Header.Description>
+          )}
         </Header>
         {frontMatter.youtubeId && (
           <div className='my-8'>
@@ -22,6 +28,44 @@ export default function Post({ frontMatter, content }) {
           </div>
         )}
         <div className='mt-4 space-y-3'>{content}</div>
+        <div className='mt-8 pt-8 border-t'>
+          <Stack grid>
+            {previous && (
+              <Stack.Item>
+                <Card>
+                  <Card.Eyebrow as='p'>Previous</Card.Eyebrow>
+                  <Card.Title>
+                    <Link
+                      as={`/screencasts/${previous.slug}`}
+                      href={`/screencasts/[slug]`}
+                    >
+                      <a className='hover:text-blue transition-colors'>
+                        {previous.title}
+                      </a>
+                    </Link>
+                  </Card.Title>
+                </Card>
+              </Stack.Item>
+            )}
+            {next && (
+              <Stack.Item>
+                <Card>
+                  <Card.Eyebrow as='p'>Next</Card.Eyebrow>
+                  <Card.Title>
+                    <Link
+                      as={`/screencasts/${next.slug}`}
+                      href={`/screencasts/[slug]`}
+                    >
+                      <a className='hover:text-blue transition-colors'>
+                        {next.title}
+                      </a>
+                    </Link>
+                  </Card.Title>
+                </Card>
+              </Stack.Item>
+            )}
+          </Stack>
+        </div>
       </article>
     </Page>
   );
--- pages/screencasts/[slug].tsx
+++ pages/screencasts/[slug].tsx
@@ -2,25 +2,24 @@ import fs from "fs";
 import matter from "gray-matter";
 import hydrate from "next-mdx-remote/hydrate";
 import renderToString from "next-mdx-remote/render-to-string";
-// import prism from "mdx-prism";
 import path from "path";
 import { screencastFilePaths, SCREENCASTS_PATH } from "../../lib/mdxUtils";
 import Article from "@/components/Article";
 import MDXComponents from "@/components/MDXComponents";
+import getContent from "@/lib/getContent";
 
-export default function Screencast({ source, frontMatter }) {
+export default function Screencast({ source, frontMatter, ...rest }) {
   const content = hydrate(source, { components: MDXComponents });
-  return <Article frontMatter={frontMatter} content={content} />;
+  return <Article frontMatter={frontMatter} content={content} {...rest} />;
 }
 
 export const getStaticProps = async ({ params }) => {
-  const screencastFilePath = path.join(SCREENCASTS_PATH, `${params.slug}.mdx`);
-  const source = fs.readFileSync(screencastFilePath);
-
-  const { content, data } = matter(source);
+  const screencasts = getContent("screencasts");
+  const screencastIndex = screencasts.findIndex((x) => x.slug === params.slug);
+  const screencast = screencasts[screencastIndex];
+  const { content, ...data } = screencast;
 
   const mdxSource = await renderToString(content, {
-    // Optionally pass remark/rehype plugins
     mdxOptions: {
       remarkPlugins: [],
       rehypePlugins: [],
@@ -30,6 +29,8 @@ export const getStaticProps = async ({ params }) => {
 
   return {
     props: {
+      previous: screencasts[screencastIndex + 1] || null,
+      next: screencasts[screencastIndex - 1] || null,
       source: mdxSource,
       frontMatter: data,
     },
@@ -37,14 +38,8 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export const getStaticPaths = async () => {
-  const paths = screencastFilePaths
-    // Remove file extensions for page paths
-    .map((path) => path.replace(/\.mdx?$/, ""))
-    // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }));
-
   return {
-    paths,
+    paths: getContent("screencasts").map((x) => `/screencasts/${x.slug}`),
     fallback: false,
   };
 };
