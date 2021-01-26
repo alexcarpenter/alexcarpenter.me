d21574aa949c9ec3c070e8182440023bb1b64ca2
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-01-26 08:11:47 -0500

    convert searchinput to use downshift

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -1,47 +1,71 @@
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-  ComboboxOptionText,
-} from '@reach/combobox';
-// import '@reach/combobox/styles.css';
+import * as React from 'react';
+import { useCombobox } from 'downshift';
 import { Search } from 'react-feather';
 import data from '@/data/search.json';
+import clsx from 'clsx';
 
 export default function SearchInput() {
+  const itemToString = (item) => (item ? item.title : '');
+  const [inputItems, setInputItems] = React.useState(data);
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    itemToString,
+    onInputValueChange: ({ inputValue }) => {
+      setInputItems(
+        data.filter((item) =>
+          itemToString(item).toLowerCase().startsWith(inputValue.toLowerCase()),
+        ),
+      );
+    },
+  });
   return (
-    <Combobox
-      aria-label=""
-      className="mt-2"
-      openOnFocus
-      onSelect={(val) => alert(val)}
-    >
-      <div className="relative">
-        <label htmlFor="search" className="sr-only">
-          Search
-        </label>
-        <ComboboxInput
-          className="border border-gray-300 rounded bg-white p-3 pr-8 w-full focus:outline-none focus:ring"
-          placeholder="Search by tag or name..."
-        />
-        <Search
-          width="1em"
-          className="text-gray-600 absolute top-1/2 transform -translate-y-1/2 right-3"
-        />
+    <div className="relative">
+      <div {...getComboboxProps()}>
+        <div className="relative">
+          <label htmlFor="search" className="sr-only" {...getLabelProps()}>
+            Search
+          </label>
+          <input
+            {...getInputProps()}
+            className="border border-gray-300 rounded bg-white p-3 pr-8 w-full focus:outline-none focus:ring"
+            placeholder="Search by tag or name..."
+          />
+          <Search
+            width="1em"
+            className="text-gray-600 absolute top-1/2 transform -translate-y-1/2 right-3"
+          />
+        </div>
       </div>
-      <ComboboxPopover className="bg-white mt-2 rounded border border-gray-300 max-h-64 overflow-y-auto">
-        <ComboboxList className="divide-y">
-          {data.map((item) => (
-            <ComboboxOption
-              key={item.slug}
-              className="p-3 hover:bg-gray-100 focus:bg-gray-100"
-              value={item.title}
-            />
+      <ul
+        {...getMenuProps()}
+        className={clsx([
+          'divide-y overflow-y-auto max-h-80 rounded mt-3 border border-gray-300',
+          isOpen && inputItems.length ? 'opacity-100' : 'opacity-0',
+        ])}
+      >
+        {isOpen &&
+          inputItems.map((item, index) => (
+            <li
+              key={`${item}${index}`}
+              className={clsx([
+                'p-3',
+                highlightedIndex === index ? 'bg-blue text-white' : 'bg-white',
+              ])}
+              {...getItemProps({ item, index })}
+            >
+              {item.title}
+            </li>
           ))}
-        </ComboboxList>
-      </ComboboxPopover>
-    </Combobox>
+      </ul>
+    </div>
   );
 }
