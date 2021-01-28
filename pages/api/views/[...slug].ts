1477d3f5a854583b7329df5146b6e571b6493de3
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-01-28 07:57:40 -0500

    prefix views with type

--- pages/api/views/[...slug].ts
+++ pages/api/views/[...slug].ts
@@ -1,8 +1,9 @@
 import db from '@/lib/firebase';
 
 export default async (req, res) => {
+  const slug = req.query.slug.join('/');
   if (req.method === 'POST') {
-    const ref = db.ref('views').child(req.query.slug);
+    const ref = db.ref('views').child(slug);
     const { snapshot } = await ref.transaction((currentViews) => {
       if (currentViews === null) {
         return 1;
@@ -17,7 +18,7 @@ export default async (req, res) => {
   }
 
   if (req.method === 'GET') {
-    const snapshot = await db.ref('views').child(req.query.slug).once('value');
+    const snapshot = await db.ref('views').child(slug).once('value');
     const views = snapshot.val();
 
     return res.status(200).json({ total: views });
