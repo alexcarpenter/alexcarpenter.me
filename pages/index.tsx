1d7d85688d325e3c4c845788eb8e8db3e2d1ac24
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-03-20 08:56:04 -0400

    Add gradient background

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,22 @@ import Page from '@/components/Page';
 
 export default function Home() {
   return (
-    <Page
-      meta={{
-        description:
-          "Hey, I'm Alex. User interface engineer currently interested in CSS architecture, React, TypeScript, design systems, and state machines.",
-      }}
-    >
-      <Page.Header>
-        <Page.Title>Hey, I'm Alex</Page.Title>
-        <Page.Description>
-          a detail oriented user interface engineer currently interested in CSS
-          architecture, React, TypeScript, design systems, and state machines.
-        </Page.Description>
-      </Page.Header>
-    </Page>
+    <>
+      <Page
+        meta={{
+          description:
+            "Hey, I'm Alex. User interface engineer currently interested in CSS architecture, React, TypeScript, design systems, and state machines.",
+        }}
+      >
+        <Page.Header>
+          <Page.Title>Hey, I'm Alex</Page.Title>
+          <Page.Description>
+            a detail oriented user interface engineer currently interested in
+            CSS architecture, React, TypeScript, design systems, and state
+            machines.
+          </Page.Description>
+        </Page.Header>
+      </Page>
+    </>
   );
 }
