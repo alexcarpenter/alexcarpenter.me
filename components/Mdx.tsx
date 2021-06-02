14e346521ce26f0e7570997ce377e5717f00e1e3
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-06-01 21:45:40 -0400

    MDX components

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -1,5 +1,12 @@
 import Code from '@/components/Code';
 
 export const components = {
+  blockquote: ({ children }) => (
+    <blockquote className="border-l-4 border-gray-800 border-opacity-10 pl-4">
+      {children}
+    </blockquote>
+  ),
   code: Code,
+  h2: ({ children }) => <h2 className="mt-8 text-xl">{children}</h2>,
+  h3: ({ children }) => <h3 className="mt-8">{children}</h3>,
 };
