192e149c5a804d860b7040ad942b6c351ce5147b
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-06-07 20:58:29 -0400

    Add type defs

--- components/BulletList.tsx
+++ components/BulletList.tsx
@@ -1,11 +1,22 @@
 import { cx } from '@/lib/utils';
 
+type GridCols =
+  | 'grid-cols-1'
+  | 'sm:grid-cols-1'
+  | 'md:grid-cols-1'
+  | 'grid-cols-2'
+  | 'sm:grid-cols-2'
+  | 'md:grid-cols-2'
+  | 'grid-cols-3'
+  | 'sm:grid-cols-3'
+  | 'md:grid-cols-3';
+
 function BulletList({
   children,
   cols,
 }: {
   children: React.ReactNode;
-  cols?: string[];
+  cols?: Array<GridCols>;
 }) {
   return (
     <ul className={cx('grid gap-4 !list-none !pl-0', [...cols])}>{children}</ul>
