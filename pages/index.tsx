84ae3fb40ac0c227f8449d271659051b690cc9e9
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-06-21 20:55:42 -0400

    Homepage font size

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
+import Note from '@/components/Note';
 import Page from '@/components/Page';
 
 export default function Home() {
   return (
     <Page>
       <div className="prose">
-        <h1 className="text-xl">Hey, I'm Alex.</h1>
-        <p className="md:text-xl">
+        <h1>Hey, I'm Alex.</h1>
+        <p>
           A detail oriented user interface engineer currently interested in CSS
           architecture, React, TypeScript, design systems, and state machines.
         </p>
-        <p className="md:text-xl">
+        {/* <aside className="sm:float-right sm:w-1/2 sm:ml-4 sm:mb-4">
+          <Note label="Currently" variant="orange">
+            Learning TypeScript via Ultimate Courses, rebuilding{' '}
+            <a href="https://us-coffee-roasters.com">us-coffee-roasters.com</a>{' '}
+            with Next.js, and playing lots of disc golf.
+          </Note>
+        </aside> */}
+        <p>
           This site is my digital playground that I have been tending on and off
           for the past eight years. I've used it as a tool to experiment and
           teach myself new tools and techniques.
         </p>
-        <p className="md:text-xl">
+        <p>
           Most recently this site is built using Next.js, Tailwind CSS,
           TypeScript, MDX, and hosted using Vercel. Overkill for a small
           personal website with a few posts I know, but I have learned a lot
