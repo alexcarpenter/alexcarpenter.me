8c4c67c774c05b1e860a64070419ee524c34370f
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-06-21 20:53:20 -0400

    Section component

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,20 +1,23 @@
-function Section({ children }) {
+function Section({
+  heading,
+  children,
+}: {
+  heading?: string;
+  children: React.ReactNode;
+}) {
   return (
     <section className="section">
       <style jsx>{`
         .section + .section {
-          @apply mt-8;
+          margin-top: 2rem;
+          padding-top: 2rem;
+          border-top: 1px solid rgba(255, 255, 255, 0.1);
         }
       `}</style>
+      {heading && <h2 className="text-xl mb-8">{heading}</h2>}
       {children}
     </section>
   );
 }
-
-function SectionHeading({ children }) {
-  return <h2 className="text-xl">{children}</h2>;
-}
-
-Section.Heading = SectionHeading;
 
 export default Section;
