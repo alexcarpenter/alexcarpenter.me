41858492a40f3fc0294b9f61b8e8be5a67e8b2c9
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-07-15 15:52:55 -0400

    No important

--- components/List.tsx
+++ components/List.tsx
@@ -1,13 +1,13 @@
 function List({ children }) {
   return (
-    <ul className="!list-none !pl-0 -my-8 divide-y divide-white divide-opacity-10">
+    <ul className="list-none pl-0 -my-8 divide-y divide-white divide-opacity-10">
       {children}
     </ul>
   );
 }
 
 function ListItem({ children }) {
-  return <li className="!py-8">{children}</li>;
+  return <li className="py-8">{children}</li>;
 }
 
 List.Item = ListItem;
