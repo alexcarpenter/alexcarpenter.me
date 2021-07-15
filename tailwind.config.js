e392b8648dff16ee279525132e82cd21fd477ad0
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-07-15 09:47:34 -0400

    Fix tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+  mode: 'jit',
   purge: {
     content: ['./pages/**/*.tsx', './components/**/*.tsx', './data/**/*.ts'],
   },
