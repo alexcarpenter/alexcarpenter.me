48d40388e0a95c947fa584300650c4aa10f578d9
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-16 08:41:44 -0400

    Animate dialog

--- components/Dialog/index.tsx
+++ components/Dialog/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as style from './style.css';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useOverlay, usePreventScroll, useModal } from '@react-aria/overlays';
 import { useDialog } from '@react-aria/dialog';
 import { FocusScope } from '@react-aria/focus';
@@ -21,19 +22,30 @@ export default function Dialog(props) {
   let { dialogProps, titleProps } = useDialog(props, ref);
 
   return (
-    <div className={style.underlay} {...underlayProps}>
-      <FocusScope contain restoreFocus autoFocus>
-        <div
-          {...overlayProps}
-          {...dialogProps}
-          {...modalProps}
-          ref={ref}
-          className={style.dialog}
-        >
-          <h3 {...titleProps}>{title}</h3>
-          {children}
-        </div>
-      </FocusScope>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        {...underlayProps}
+        className={style.underlay}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <FocusScope contain restoreFocus autoFocus>
+          <motion.div
+            {...overlayProps}
+            {...dialogProps}
+            {...modalProps}
+            ref={ref}
+            className={style.dialog}
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.5, opacity: 0 }}
+          >
+            <h3 {...titleProps}>{title}</h3>
+            {children}
+          </motion.div>
+        </FocusScope>
+      </motion.div>
+    </AnimatePresence>
   );
 }
