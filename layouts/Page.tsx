bc7ae3819ff5a807cd4cbe65db64a0379d66d59b
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-15 22:22:05 -0400

    Remove layouts

--- layouts/Page.tsx
+++
@@ -1,7 +0,0 @@
-import Base from './Base';
-
-const Page: React.FC = ({ children }) => {
-  return <Base>{children}</Base>;
-};
-
-export default Page;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,12 @@
-import { ThemeProvider } from 'next-themes';
-import type { ReactElement, ReactNode } from 'react';
-import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import '@/styles/global.css';
+import { ThemeProvider } from 'next-themes';
 
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+    <ThemeProvider disableTransitionOnChange>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
