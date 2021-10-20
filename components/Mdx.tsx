f9895d85046e3a54c6d185bd6c2216c78d0c5c79
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-10-20 11:42:57 -0400

    Swipeable tabs post

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -1,7 +1,26 @@
 import Code from '@/components/Code';
 import Note from '@/components/Note';
 
+function CodeSandbox({ src }) {
+  return (
+    <iframe
+      src={src}
+      style={{
+        width: '100%',
+        height: 500,
+        border: 0,
+        borderRadius: 4,
+        overflow: 'hidden',
+      }}
+      title="swipeable-tabs-3"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    ></iframe>
+  );
+}
+
 export const components = {
   code: Code,
   Note,
+  CodeSandbox,
 };
