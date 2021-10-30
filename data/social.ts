512b22093932563e0ce595a3b955c746ba55ef37
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-10-30 09:11:23 -0400

    chore: reorganize social links

--- data/social.ts
+++ data/social.ts
@@ -1,22 +1,22 @@
 export default [
   {
-    children: 'Email',
-    href: 'mailto:im.alexcarpenter@gmail.com',
-  },
-  {
-    children: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/imalexcarpenter/',
+    children: 'Github',
+    href: 'https://github.com/alexcarpenter',
   },
   {
     children: 'Twitter',
     href: 'https://twitter.com/hybrid_alex',
   },
   {
-    children: 'Github',
-    href: 'https://github.com/alexcarpenter',
+    children: 'Email',
+    href: 'mailto:im.alexcarpenter@gmail.com',
   },
   {
     children: 'Instagram',
     href: 'https://www.instagram.com/alexcarp/',
+  },
+  {
+    children: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/imalexcarpenter/',
   },
 ];
