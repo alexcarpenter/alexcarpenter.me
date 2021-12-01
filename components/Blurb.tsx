bfd941da69766dd9a202c80d81e97ba6f1359ef2
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-30 19:11:00 -0500

    update homepage

---
+++ components/Blurb.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { formatDate } from '@/lib/utils';
+
+interface BlurbProps {
+  date: string;
+  children: string | React.ReactNode;
+}
+
+export default function Blurb({ date, children }: BlurbProps) {
+  return (
+    <article className="relative bg-white dark:bg-gray-800 w-full p-4 rounded-md border borderColor">
+      <span className="block w-4 h-4 bg-white dark:bg-gray-800 absolute top-0 left-4 rotate-45 -translate-y-1/2 border borderColor border-r-0 border-b-0" />
+      <p className="prose">{children}</p>
+      <p className="mt-4 text-sm textSecondary">
+        Updated {formatDate(date, 'long')}
+      </p>
+    </article>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,70 @@
-import Link from 'next/link';
-import Note from '@/components/Note';
+import Image from 'next/image';
+import Blurb from '@/components/Blurb';
+import Link from '@/components/Link';
 import Page from '@/components/Page';
 
 export default function Home() {
   return (
     <Page>
-      <div className="prose">
-        <h1>Hey, I'm Alex.</h1>
-        <p>
-          A detail oriented user interface engineer interested in CSS
-          architecture, React, TypeScript, design systems, and state machines.
-          Currently working at{' '}
-          <Link href="/work/hashicorp">
-            <a>HashiCorp</a>
-          </Link>
-          .
-        </p>
-        <aside className="sm:float-right sm:w-1/2 sm:ml-4 sm:mb-4">
-          <Note label="Currently" variant="orange">
-            Learning TypeScript via Ultimate Courses, rebuilding{' '}
-            <a href="https://us-coffee-roasters.com">us-coffee-roasters.com</a>{' '}
-            with Next.js, and playing lots of disc golf.
-          </Note>
-        </aside>
-        <p>
-          This site is my digital playground that I have been tending on and off
-          for the past eight years. I've used it as a tool to experiment and
-          teach myself new tools and techniques.
-        </p>
-        <p>
-          Most recently this site is built using Next.js, Tailwind CSS,
-          TypeScript, MDX, and hosted using Vercel. Overkill for a small
-          personal website with a few posts I know, but I have learned a lot
-          about React, TypeScript, Cypress E2E tests, and Github Actions from
-          this experiment. Feel free to check out the source code on{' '}
-          <a href="https://github.com/alexcarpenter/alexcarpenter-next">
-            Github
-          </a>
-          .
-        </p>
+      <h1 className="sr-only">Alex Carpenter</h1>
+      <Blurb date="2021-11-30T17:19:32Z">
+        The winter snow has finally arrived here in Michigan. Not sure I am
+        ready for it but excited for the holidays!
+      </Blurb>
+      <div className="mt-8">
+        <div className="prose">
+          <h2>About</h2>
+          <div
+            className="relative float-right w-1/3 ml-8 mb-8 inline-flex rounded-md overflow-hidden"
+            style={{
+              aspectRatio: '1/1',
+            }}
+          >
+            <Image
+              src="/images/family.jpg"
+              layout="fill"
+              alt="My wife, my dog, and I at the park."
+            />
+          </div>
+          <p>
+            Hey! I'm Alex, a detail oriented user interface engineer interested
+            in CSS architecture, React, TypeScript, design systems, and state
+            machines. Currently working at{' '}
+            <Link href="/work/hashicorp">HashiCorp</Link>, helping build and
+            maintain public-facing HashiCorp websites and web applications with
+            Next.js.
+          </p>
+          <p>
+            Outside of work I am a self-proclaimed serial hobbyist. Currently
+            interested in <del>fly fishing</del>, <del>RC cars</del>, disc golf,
+            and outdoor cooking{' '}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.instagram.com/stories/highlights/17960504368004615/"
+            >
+              #smokedmeats
+            </a>
+            .
+          </p>
+          <h2>Colophon</h2>
+          {/* <p>
+            This site is my digital playground that I have been tending on and
+            off for the past eight years. I've used it as a tool to experiment
+            and teach myself new tools and techniques.
+          </p> */}
+          <p>
+            This site is built using Next.js, Tailwind CSS, TypeScript, MDX, and
+            hosted using Vercel. Type is set in JetBrains Mono. Overkill for a
+            small personal website with a few posts I know, but I have learned a
+            lot about React, TypeScript, Cypress E2E tests, and Github Actions
+            from this experiment. Feel free to check out the source code on{' '}
+            <a href="https://github.com/alexcarpenter/alexcarpenter-next">
+              Github
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </Page>
   );
