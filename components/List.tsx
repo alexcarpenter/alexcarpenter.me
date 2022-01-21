e2f8f71544725c1b5565243f514fb0aaac7a9459
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-21 08:47:05 -0500

    fix: border colors

--- components/List.tsx
+++ components/List.tsx
@@ -5,7 +5,7 @@ function List({ children }) {
       className={cx(
         'list-none pl-0 -my-12 divide-y',
         ['divide-gray-200'],
-        ['dark:divide-gray-800'],
+        ['dark:divide-gray-700'],
       )}
     >
       {children}
--- components/YoutubeEmbed.tsx
+++ components/YoutubeEmbed.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import { ReactYouTubeLite } from 'react-youtube-lite';
 
 function YoutubeEmbed({ id }) {
-  return <ReactYouTubeLite url={`https://www.youtube.com/watch?v=${id}`} />;
+  return (
+    <div className="border borderColor">
+      <ReactYouTubeLite url={`https://www.youtube.com/watch?v=${id}`} />
+    </div>
+  );
 }
 
 export default YoutubeEmbed;
