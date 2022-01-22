073bbac211904546d2088bcd9a74f66d82bce1f7
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-22 16:20:30 -0500

    chore: tailwind setup

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider
+      disableTransitionOnChange
+      defaultTheme="system"
+      attribute="class"
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <p>Hello world</p>;
+};
+
+export default Home;
