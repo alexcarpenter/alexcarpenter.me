d8d838e6ed6b309686e049c61ccf38176cd09c13
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-23 10:02:16 -0500

    feat: work page

---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+import { cx } from "@/lib/utils";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "sm";
+}
+
+export default function Button({ size, children, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cx(
+        "border px-4 py-1 rounded-md",
+        "bg-gray-50",
+        "dark:bg-gray-800",
+        "borderColor",
+        {
+          "text-sm": size === "sm",
+        }
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
--- components/Section.tsx
+++ components/Section.tsx
@@ -5,7 +5,8 @@ type SectionProps = {
 const Section: React.FC<SectionProps> = ({ heading, children }) => {
   return (
     <section className="section grid gap-12">
-      {heading ? <h2 className="text-xl">{heading}</h2> : null} {children}
+      {heading ? <h2 className="text-xl">{heading}</h2> : null}{" "}
+      <div>{children}</div>
     </section>
   );
 };
