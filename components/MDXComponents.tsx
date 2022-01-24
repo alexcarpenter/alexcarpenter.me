0905f67292008cd4c7a15c2ec2cf0267a578995f
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-24 08:40:31 -0500

    chore: add codesandbox component

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -11,6 +11,22 @@ import Video from "@/components/Video";
 
 export const components = {
   Card,
+  CodeSandbox: ({ src, title }: { src: string; title: string }) => (
+    <iframe
+      title={title}
+      src={src}
+      className="my-12"
+      style={{
+        width: "100%",
+        height: 500,
+        border: 0,
+        borderRadius: 4,
+        overflow: "hidden",
+      }}
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    ></iframe>
+  ),
   ExternalLink,
   Image,
   Highlights,
