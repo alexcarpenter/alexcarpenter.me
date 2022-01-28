66fff3b3a17a3714f9bfea358a1ed6f6064d7269
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-28 08:16:17 -0500

    fix(a11y): nav reduced motion

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,14 @@
-import * as React from "react";
-import { motion, AnimateSharedLayout } from "framer-motion";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { cx } from "@/lib/utils";
-import navData from "@/data/nav.json";
+import * as React from 'react';
+import { motion, AnimateSharedLayout, useReducedMotion } from 'framer-motion';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { cx } from '@/lib/utils';
+import navData from '@/data/nav.json';
 
 export default function Nav() {
   const { pathname } = useRouter();
   const [activeIndex, setActiveIndex] = React.useState<number | null>(null);
+  const shouldReduceMotion = useReducedMotion();
   return (
     <nav className="relative -mx-2 -my-1">
       <AnimateSharedLayout>
@@ -21,24 +22,20 @@ export default function Nav() {
               <motion.li key={index} onHoverStart={() => setActiveIndex(index)}>
                 <Link href={item.path}>
                   <a
-                    className={cx(
-                      "relative block px-2 py-1"
-                      // ["text-gray-600 hover:text-gray-700"],
-                      // ["dark:text-gray-300 dark:hover:text-white"]
-                    )}
-                    aria-current={pathname === item.path ? "page" : undefined}
+                    className={cx('relative block px-2 py-1')}
+                    aria-current={pathname === item.path ? 'page' : undefined}
                   >
                     <span className="relative z-10">{item.label}</span>
                     {isActive && (
                       <motion.span
                         layoutId="shadow"
                         transition={{
-                          duration: 0.2,
+                          duration: shouldReduceMotion ? 0 : 0.2,
                         }}
                         className={cx(
-                          "absolute inset-0 rounded-md pointer-events-none z-0",
-                          ["bg-gray-200"],
-                          ["dark:bg-gray-800"]
+                          'absolute inset-0 rounded-md pointer-events-none z-0',
+                          ['bg-gray-200'],
+                          ['dark:bg-gray-800'],
                         )}
                       />
                     )}
