b63bdfd3df5e1f61fc050d041d2ff357b95943be
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-02-26 08:05:36 -0500

    chore: forwardref button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,30 @@
+import * as React from 'react';
 import { cx } from '@/lib/utils';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'sm';
 }
 
-export default function Button({ size, children, ...props }: ButtonProps) {
-  return (
-    <button
-      className={cx(
-        'border px-4 py-1 rounded-md',
-        'bg-gray-50 border-gray-200',
-        'dark:bg-gray-800 dark:border-gray-700',
-        {
-          'text-sm': size === 'sm',
-        },
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { size, children, ...rest } = props;
+    return (
+      <button
+        className={cx(
+          'border px-4 py-1 rounded-md',
+          'bg-gray-50 border-gray-200',
+          'dark:bg-gray-800 dark:border-gray-700',
+          {
+            'text-sm': size === 'sm',
+          },
+        )}
+        {...rest}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+export default Button;
