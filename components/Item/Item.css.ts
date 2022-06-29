984b6dd88ffbdb1cd03d352c66953a66368b0e79
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-06-29 08:26:07 -0400

    responsive item

--- components/Item/Item.css.ts
+++ components/Item/Item.css.ts
@@ -3,16 +3,29 @@ import { style } from "@vanilla-extract/css";
 
 export const root = style({
   display: "grid",
-  gridTemplateColumns: "8rem 1fr",
+  gridTemplateColumns: "1fr",
   columnGap: tokens.spacing.xl,
+  "@media": {
+    "screen and (min-width: 500px)": {
+      gridTemplateColumns: "8rem 1fr",
+    },
+  },
 });
 
 export const meta = style({
-  gridColumnStart: 1,
-  gridRowStart: 1,
+  "@media": {
+    "screen and (min-width: 500px)": {
+      gridColumnStart: 1,
+      gridRowStart: 1,
+    },
+  },
 });
 
 export const content = style({
-  gridColumnStart: 2,
-  gridRowStart: 1,
+  "@media": {
+    "screen and (min-width: 500px)": {
+      gridRowStart: 1,
+      gridColumnStart: 2,
+    },
+  },
 });
