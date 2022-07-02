6275e16173247f7e9112347719f9e9a9fe3a05ed
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-07-02 08:39:40 -0400

    rework spacer props

--- components/Spacer/Spacer.tsx
+++ components/Spacer/Spacer.tsx
@@ -1,11 +1,10 @@
-import type { Sprinkles } from "@/styles/sprinkle.css";
+import { PartialPick } from "types";
+import type { BoxProps } from "@/components/Box";
 import { Box } from "@/components/Box";
 
-interface SpacerProps {
-  height?: Sprinkles["height"];
-  width?: Sprinkles["width"];
-}
-
-export const Spacer = ({ height, width }: SpacerProps) => {
+export const Spacer = ({
+  height,
+  width,
+}: PartialPick<BoxProps<"span">, "width" | "height">) => {
   return <Box as="span" display="block" height={height} width={width} />;
 };
