0cbc0eb0b655e217b504f88e8f3ccf2db2001514
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-07-02 08:20:01 -0400

    partialpick

--- components/Spacer/Spacer.tsx
+++ components/Spacer/Spacer.tsx
@@ -1,7 +1,8 @@
+import type { PartialPick } from "types";
 import type { BoxProps } from "@/components/Box";
 import { Box } from "@/components/Box";
 
-interface SpacerProps extends Pick<BoxProps, "width" | "height"> {}
+interface SpacerProps extends PartialPick<BoxProps, "width" | "height"> {}
 
 export const Spacer = ({ height, width }: SpacerProps) => {
   return <Box as="span" display="block" height={height} width={width} />;
