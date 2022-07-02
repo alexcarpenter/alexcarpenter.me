b7e0a113982ffedb7045ae9a95c9fcae3e475944
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-07-02 08:32:57 -0400

    fix spacer types

--- components/Spacer/Spacer.tsx
+++ components/Spacer/Spacer.tsx
@@ -1,8 +1,10 @@
-import type { PartialPick } from "types";
-import type { BoxProps } from "@/components/Box";
+import type { Sprinkles } from "@/styles/sprinkle.css";
 import { Box } from "@/components/Box";
 
-interface SpacerProps extends PartialPick<BoxProps, "width" | "height"> {}
+interface SpacerProps {
+  height?: Sprinkles["height"];
+  width?: Sprinkles["width"];
+}
 
 export const Spacer = ({ height, width }: SpacerProps) => {
   return <Box as="span" display="block" height={height} width={width} />;
