ca5f4f50d29e835c996959cbc85005669592cdaf
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-08-08 08:47:49 -0400

    add redirect

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/posts/jon-gaffney-edc-winter-2021",
+        destination:
+          "https://gear.alexcarpenter.me/posts/jon-gaffney-edc-winter-2021",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(withVanillaExtract(nextConfig));
