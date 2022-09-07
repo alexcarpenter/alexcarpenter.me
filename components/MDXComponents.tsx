21cef38d9e86f94ebeed6c167ce122e26a87a44d
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-09-07 17:19:44 -0400

    fix: MDXComponents

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
-import { Link, LinkProps } from "@/components/Link";
-import { Heading, HeadingProps } from "@/components/Heading";
 
 export const Components = {
   Image,
-  a: (props: LinkProps) => <Link {...props} />,
-  h2: (props: HeadingProps) => <Heading as="h2" {...props} />,
-  h3: (props: HeadingProps) => <Heading as="h3" {...props} />,
 };
