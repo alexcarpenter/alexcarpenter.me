158e2e94c78858b7004f077262ec7b1eb730174f
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-09-12 07:50:51 -0400

    increase list spacing

--- components/List/List.css.ts
+++ components/List/List.css.ts
@@ -9,6 +9,6 @@ export const item = style({});
 globalStyle(`${root} li + li`, {
   borderTop: "1px solid",
   borderColor: themeVars.color.border,
-  marginBlockStart: tokens.spacing.xl,
-  paddingBlockStart: tokens.spacing.xl,
+  marginBlockStart: tokens.spacing.xxl,
+  paddingBlockStart: tokens.spacing.xxl,
 });
