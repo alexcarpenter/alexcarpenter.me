a51d8a0ea883a7f296e64dd712dd9ee97331d75a
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-09-19 17:38:27 -0400

    fix border color

--- components/List/List.css.ts
+++ components/List/List.css.ts
@@ -7,8 +7,7 @@ export const root = style({});
 export const item = style({});
 
 globalStyle(`${root} > li + li`, {
-  borderTop: "1px solid",
-  borderColor: themeVars.color.border,
+  borderTop: `1px solid ${themeVars.color.border}`,
   marginBlockStart: tokens.spacing.xxl,
   paddingBlockStart: tokens.spacing.xxl,
 });
