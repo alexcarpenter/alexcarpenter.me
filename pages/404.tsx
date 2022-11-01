2725014e569bf26487312ac60db54b4c85f7525e
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-11-01 08:22:41 -0400

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,33 @@
 import * as React from "react";
 import type { NextPage } from "next";
+import { Box } from "components/Box";
+import { Heading } from "components/Heading";
+import { Text } from "components/Text";
+import { Spacer } from "components/Spacer";
 
 const FourOhFour: NextPage = () => {
   return (
     <>
-      <h1>404 - Page not found</h1>
+      <Box
+        as="header"
+        textAlign={{ md: "center" }}
+        maxWidth="container"
+        marginX="auto"
+      >
+        <Heading fontSize={{ xs: "xxl", sm: "xxxl" }} as="h1">
+          404
+        </Heading>
+        <Spacer height="xl" />
+        <Text
+          fontSize={{ xs: "lg", sm: "xl" }}
+          color="foregroundNeutral"
+          style={{
+            display: "inline-flex",
+          }}
+        >
+          Page not found
+        </Text>
+      </Box>
     </>
   );
 };
