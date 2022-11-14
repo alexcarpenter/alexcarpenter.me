1dfeef5a12376203624a59425122df88667041e8
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-11-14 18:16:40 -0500

    use next link

--- components/ActivityItem/ActivityItem.tsx
+++ components/ActivityItem/ActivityItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { Activity } from "contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { Text } from "components/Text";
@@ -8,7 +9,15 @@ const ActivityItem = ({ date, body }: Activity) => {
   return (
     <article>
       <div className={styles.content}>
-        <MDXContent components={{}} />
+        <MDXContent
+          components={{
+            a: (props: any) => (
+              <Link href={props.href}>
+                <a>{props.children}</a>
+              </Link>
+            ),
+          }}
+        />
       </div>
       <Text as="time" dateTime={date} color="foregroundNeutral">
         {new Date(date).toLocaleDateString("en-US", {
