9a5a636bd730c7f3459706c4013a78ef2f74527b
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-12-30 09:46:22 -0500

    Carousel -> ImageCarousel

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,13 +1,13 @@
 import slugify from "@sindresorhus/slugify";
 import Image from "next/legacy/image";
-import { Carousel } from "components/Carousel";
+import { ImageCarousel } from "components/ImageCarousel";
 import { Heading } from "components/Heading";
 import { Link } from "components/Link";
 import { Text } from "components/Text";
 import { YoutubeEmbed } from "components/YoutubeEmbed";
 
 export const Components = {
-  Carousel,
+  ImageCarousel,
   Image,
   YoutubeEmbed,
   a: (props: any) => <Link {...props} variant="highlight" />,
