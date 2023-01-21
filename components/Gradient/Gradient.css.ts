774057ac862b772a26ddb91199f633c23c940e7b
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-01-21 12:28:48 -0500

    gradient tweaks

--- components/Gradient/Gradient.css.ts
+++ components/Gradient/Gradient.css.ts
@@ -8,6 +8,11 @@ export const root = style({
   right: 0,
   maxWidth: 1200,
   transform: "translate3d(0, -50%, 0)",
+  "@media": {
+    "screen and (max-width: 768px)": {
+      display: "none",
+    },
+  },
 });
 
 export const blob = style({
@@ -18,21 +23,13 @@ export const blob = style({
 });
 
 export const left = style({
+  transform: "translate3d(150px, 0, 0)",
   background: "linear-gradient(to right, #63d0ff, #844fba)",
   opacity: 0.6,
-  "@media": {
-    "screen and (min-width: 768px)": {
-      transform: "translate3d(150px, 0, 0)",
-    },
-  },
 });
 
 export const right = style({
+  transform: "translate3d(-150px, 0, 0)",
   background: "linear-gradient(to right, #ffb2b8, #c74fb8)",
   opacity: 0.3,
-  "@media": {
-    "screen and (min-width: 768px)": {
-      transform: "translate3d(-150px, 0, 0)",
-    },
-  },
 });
--- components/Gradient/Gradient.tsx
+++ components/Gradient/Gradient.tsx
@@ -1,9 +1,10 @@
 import { cn } from "lib/utils";
+import { motion } from "framer-motion";
 import * as styles from "./Gradient.css";
 
 const Gradient = () => {
   return (
-    <div className={styles.root}>
+    <div className={styles.root} aria-hidden>
       <span className={cn(styles.blob, styles.left)} />
       <span className={cn(styles.blob, styles.right)} />
     </div>
