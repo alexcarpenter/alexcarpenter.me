84f2e87ece543930e26d3dd9e0bd8b866a5cf6e0
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-02-07 09:50:05 -0500

    back to neutral

--- components/Link/Link.css.ts
+++ components/Link/Link.css.ts
@@ -3,9 +3,14 @@ import { vars } from "styles/vars.css";
 
 export const root = style({
   textDecoration: "underline",
-  textDecorationColor: vars.color.underline,
-  ":visited": {
-    textDecorationColor: vars.color.underlineVisited,
+  textDecorationColor: vars.color.underlineNeutral,
+  textDecorationThickness: 1.5,
+  textUnderlineOffset: 1.5,
+  transition: "ease-in-out",
+  transitionDuration: vars.transition.duration,
+  transitionProperty: "text-decoration-color",
+  ":hover": {
+    textDecorationColor: "inherit",
   },
   ":focus": {
     outline: "transparent",
