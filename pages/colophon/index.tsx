90a3389f6edceb0fa9111c5f7181c88c3103fd72
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-02-23 08:31:11 -0500

    chore: add section heading

--- pages/colophon/index.tsx
+++ pages/colophon/index.tsx
@@ -29,119 +29,144 @@ const Colophon: NextPage = () => {
 
       <Spacer height="xxxl" />
 
-      <List.Container>
-        <List.Item>
-          <Grid.Container>
-            <Grid.Column colStart={{ xs: "1" }} colEnd={{ xs: "-1", md: "1" }}>
-              <Heading>Next.js</Heading>
-            </Grid.Column>
+      <section>
+        <Grid.Container>
+          <Grid.Column
+            colStart={{ xs: "1", md: "2" }}
+            colEnd={{ xs: "-1", md: "4" }}
+          >
+            <Heading fontSize="lg">Tech stack &not;</Heading>
+          </Grid.Column>
+        </Grid.Container>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "2" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quisquam blanditiis totam velit exercitationem labore eum
-                voluptatem ipsum earum? Commodi, distinctio?
-              </Text>
-            </Grid.Column>
+        <Spacer height="xl" />
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "4" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text color="foregroundNeutral" fontSize="sm">
-                <a href="https://nextjs.org/">Visit ↗</a>
-              </Text>
-            </Grid.Column>
-          </Grid.Container>
-        </List.Item>
+        <List.Container>
+          <List.Item>
+            <Grid.Container>
+              <Grid.Column
+                colStart={{ xs: "1" }}
+                colEnd={{ xs: "-1", md: "1" }}
+              >
+                <Heading as="h3">Next.js</Heading>
+              </Grid.Column>
 
-        <List.Item>
-          <Grid.Container>
-            <Grid.Column colStart={{ xs: "1" }} colEnd={{ xs: "-1", md: "1" }}>
-              <Heading>vanilla-extract</Heading>
-            </Grid.Column>
+              <Grid.Column
+                colStart={{ xs: "1", md: "2" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam blanditiis totam velit exercitationem labore eum
+                  voluptatem ipsum earum? Commodi, distinctio?
+                </Text>
+              </Grid.Column>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "2" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quisquam blanditiis totam velit exercitationem labore eum
-                voluptatem ipsum earum? Commodi, distinctio?
-              </Text>
-            </Grid.Column>
+              <Grid.Column
+                colStart={{ xs: "1", md: "4" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text color="foregroundNeutral" fontSize="sm">
+                  <a href="https://nextjs.org/">Visit ↗</a>
+                </Text>
+              </Grid.Column>
+            </Grid.Container>
+          </List.Item>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "4" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text color="foregroundNeutral" fontSize="sm">
-                <a href="https://vanilla-extract.style/">Visit ↗</a>
-              </Text>
-            </Grid.Column>
-          </Grid.Container>
-        </List.Item>
+          <List.Item>
+            <Grid.Container>
+              <Grid.Column
+                colStart={{ xs: "1" }}
+                colEnd={{ xs: "-1", md: "1" }}
+              >
+                <Heading as="h3">vanilla-extract</Heading>
+              </Grid.Column>
 
-        <List.Item>
-          <Grid.Container>
-            <Grid.Column colStart={{ xs: "1" }} colEnd={{ xs: "-1", md: "1" }}>
-              <Heading>Radix UI</Heading>
-            </Grid.Column>
+              <Grid.Column
+                colStart={{ xs: "1", md: "2" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam blanditiis totam velit exercitationem labore eum
+                  voluptatem ipsum earum? Commodi, distinctio?
+                </Text>
+              </Grid.Column>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "2" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quisquam blanditiis totam velit exercitationem labore eum
-                voluptatem ipsum earum? Commodi, distinctio?
-              </Text>
-            </Grid.Column>
+              <Grid.Column
+                colStart={{ xs: "1", md: "4" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text color="foregroundNeutral" fontSize="sm">
+                  <a href="https://vanilla-extract.style/">Visit ↗</a>
+                </Text>
+              </Grid.Column>
+            </Grid.Container>
+          </List.Item>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "4" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text color="foregroundNeutral" fontSize="sm">
-                <a href="https://www.radix-ui.com/">Visit ↗</a>
-              </Text>
-            </Grid.Column>
-          </Grid.Container>
-        </List.Item>
+          <List.Item>
+            <Grid.Container>
+              <Grid.Column
+                colStart={{ xs: "1" }}
+                colEnd={{ xs: "-1", md: "1" }}
+              >
+                <Heading as="h3">Radix UI</Heading>
+              </Grid.Column>
 
-        <List.Item>
-          <Grid.Container>
-            <Grid.Column colStart={{ xs: "1" }} colEnd={{ xs: "-1", md: "1" }}>
-              <Heading>Framer Motion</Heading>
-            </Grid.Column>
+              <Grid.Column
+                colStart={{ xs: "1", md: "2" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam blanditiis totam velit exercitationem labore eum
+                  voluptatem ipsum earum? Commodi, distinctio?
+                </Text>
+              </Grid.Column>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "2" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quisquam blanditiis totam velit exercitationem labore eum
-                voluptatem ipsum earum? Commodi, distinctio?
-              </Text>
-            </Grid.Column>
+              <Grid.Column
+                colStart={{ xs: "1", md: "4" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text color="foregroundNeutral" fontSize="sm">
+                  <a href="https://www.radix-ui.com/">Visit ↗</a>
+                </Text>
+              </Grid.Column>
+            </Grid.Container>
+          </List.Item>
 
-            <Grid.Column
-              colStart={{ xs: "1", md: "4" }}
-              colEnd={{ xs: "-1", md: "4" }}
-            >
-              <Text color="foregroundNeutral" fontSize="sm">
-                <a href="https://www.framer.com/motion/">Visit ↗</a>
-              </Text>
-            </Grid.Column>
-          </Grid.Container>
-        </List.Item>
-      </List.Container>
+          <List.Item>
+            <Grid.Container>
+              <Grid.Column
+                colStart={{ xs: "1" }}
+                colEnd={{ xs: "-1", md: "1" }}
+              >
+                <Heading as="h3">Framer Motion</Heading>
+              </Grid.Column>
+
+              <Grid.Column
+                colStart={{ xs: "1", md: "2" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam blanditiis totam velit exercitationem labore eum
+                  voluptatem ipsum earum? Commodi, distinctio?
+                </Text>
+              </Grid.Column>
+
+              <Grid.Column
+                colStart={{ xs: "1", md: "4" }}
+                colEnd={{ xs: "-1", md: "4" }}
+              >
+                <Text color="foregroundNeutral" fontSize="sm">
+                  <a href="https://www.framer.com/motion/">Visit ↗</a>
+                </Text>
+              </Grid.Column>
+            </Grid.Container>
+          </List.Item>
+        </List.Container>
+      </section>
     </>
   );
 };
