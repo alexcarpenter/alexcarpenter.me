29ba24ffed376d027e7f71943e4ae602761dfa75
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-03-03 07:51:57 -0500

    add glossy border

--- components/ThemeToggle/ThemeToggle.css.ts
+++ components/ThemeToggle/ThemeToggle.css.ts
@@ -1,4 +1,4 @@
-import { style, styleVariants } from "@vanilla-extract/css";
+import { globalStyle, style, styleVariants } from "@vanilla-extract/css";
 import { vars } from "styles/vars.css";
 
 export const root = style({
@@ -13,6 +13,18 @@ export const root = style({
   paddingLeft: vars.spacing.xxs,
   paddingRight: vars.spacing.xxs,
   borderRadius: vars.radii.full,
+});
+
+globalStyle(`.dark ${root}::before`, {
+  content: "",
+  position: "absolute",
+  inset: -1,
+  borderRadius: "inherit",
+  padding: 1,
+  backgroundImage: `linear-gradient(to bottom right, rgba(255, 255, 255, .2), ${vars.color.borderFaint}, rgba(255, 255, 255, .1))`,
+  mask: "linear-gradient(#000, #000) content-box, linear-gradient(#000, #000)",
+  maskComposite: "exclude",
+  pointerEvents: "none",
 });
 
 export const toggle = style({
