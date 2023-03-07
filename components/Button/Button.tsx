f79d70d96a74823b0f8803f258f5e5b1f99537f1
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-03-07 08:11:20 -0500

    Button (#106)
    * init Button
    
    * button interface
    
    * update link underline
    
    * add read cv link
    
    * fix typo
    
    * update inspiration

---
+++ components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import { Slot } from "@radix-ui/react-slot";
+import * as styles from "./Button.css";
+
+interface ButtonProps {
+  asChild?: boolean;
+  children?: React.ReactNode;
+}
+
+const Button = ({ asChild, ...props }: ButtonProps) => {
+  const Comp = asChild ? Slot : "button";
+  return <Comp className={styles.root} {...props} />;
+};
+
+export { Button };
