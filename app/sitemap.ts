0376f8ce7bd9d474570b85b38bab297594c2e0cd
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-06-12 10:22:04 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,22 @@
+import { allPosts, allPages } from "@/.contentlayer/generated";
+
 export default async function sitemap() {
-  const routes = ["", "/posts", "/bookmarks", "/activity", "/colophon"].map(
+  const pages = allPages.map((page) => ({
+    url: `https://alexcarpenter.me/${page.slug}`,
+    lastModified: page.updated,
+  }));
+
+  const posts = allPosts.map((post) => ({
+    url: `https://alexcarpenter.me/posts/${post.slug}`,
+    lastModified: post.date,
+  }));
+
+  const routes = ["", "/posts", "/about", "/timeline", "/colophon"].map(
     (route) => ({
       url: `https://alexcarpenter.me${route}`,
       lastModified: new Date().toISOString().split("T")[0],
     })
   );
 
-  return [...routes];
+  return [...routes, ...pages, ...posts];
 }
