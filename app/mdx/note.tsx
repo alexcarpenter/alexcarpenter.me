5e0d89a798d6512f1b4303f2cca31659bb3890be
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-07-30 07:48:17 -0400

    Update note.tsx

--- app/mdx/note.tsx
+++ app/mdx/note.tsx
@@ -7,7 +7,7 @@ const Note = ({
 }) => {
   return (
     <aside className="note relative border border-dashed">
-      <p className="absolute left-1 top-0 -translate-y-1/2 bg-page px-2 text-sm font-semibold uppercase tracking-widest text-[#F04A00] text-secondary">
+      <p className="absolute left-1 top-0 -translate-y-1/2 bg-page px-2 text-sm font-semibold uppercase tracking-widest text-secondary">
         {label || "Note"}
       </p>
       <div className="space-y-2 p-3 pt-4 text-sm">{children}</div>
