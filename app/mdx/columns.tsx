f8ae3fcf9259992244f9785ad04d8e4d6d28ffdb
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-08-17 08:19:52 -0400

    fix break inside

--- app/mdx/columns.tsx
+++ app/mdx/columns.tsx
@@ -1,5 +1,10 @@
 import { Slot, type SlotProps } from "@radix-ui/react-slot";
 
 export function Columns(props: SlotProps) {
-  return <Slot className="columns-2" {...props} />;
+  return (
+    <Slot
+      className="columns-2 gap-x-8 [&>li]:break-inside-avoid-column"
+      {...props}
+    />
+  );
 }
