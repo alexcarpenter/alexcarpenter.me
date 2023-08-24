71a75130434f217f0268471c8ae8806196470477
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-08-24 08:39:13 -0400

    US Coffee Roasters

---
+++ app/us-coffee-roasters/page.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import * as R from "remeda";
+import roasters from "./roasters.json";
+
+const page = {
+  slug: "/us-coffee-roasters",
+  title: "US Coffee Roasters",
+  description: "Crowd-sourced list of US coffee roasters",
+};
+
+export async function generateMetadata() {
+  const { slug, title, description } = page;
+
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      title,
+      description,
+      url: `https://alexcarpenter.me/${slug}`,
+    },
+    twitter: {
+      title,
+      description,
+      card: "summary",
+    },
+  };
+}
+
+export default function Roasters() {
+  const roastersByState = R.pipe(
+    roasters,
+    R.groupBy(({ state }) => state)
+  );
+  return (
+    <>
+      <header className="mb-6 border-b-2 pb-2">
+        <h1 className="font-semibold">{page.title}</h1>
+        {page.description ? (
+          <p className="text-secondary">{page.description}</p>
+        ) : null}
+      </header>
+
+      <div className="prose">
+        <dl>
+          {Object.keys(roastersByState)
+            .sort()
+            .map((state) => {
+              const _roasters = roastersByState[state];
+              return (
+                <React.Fragment key={state}>
+                  <dt>{state}</dt>
+                  <dd>
+                    <ul>
+                      {_roasters.map(({ name, address, website }) => {
+                        return (
+                          <li key={website}>
+                            <p>
+                              <a href={website}>{name}</a>
+                            </p>
+                            <p>
+                              <small>{address}</small>
+                            </p>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </dd>
+                </React.Fragment>
+              );
+            })}
+        </dl>
+      </div>
+    </>
+  );
+}
