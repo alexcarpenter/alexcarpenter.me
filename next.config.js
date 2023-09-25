61115dd98d4e62c174135316cb4ee342d10a1ebd
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-09-25 08:19:51 -0400

    bump next

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   async redirects() {
     return [
       {
