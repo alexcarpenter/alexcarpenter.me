9d2d7b5d84d65bf3b29b381f311904c5ecf79451
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-10-18 07:24:39 -0400

    filter drafts from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,17 @@
 import { allPages } from "contentlayer/generated";
 
 export default async function sitemap() {
-  const pages = allPages.map((page) => ({
-    url: `https://alexcarpenter.me/${page.slug}`,
-    lastModified: page.updated || page.published,
-  }));
+  const pages = allPages
+    .filter(({ draft }) => !draft)
+    .map((page) => ({
+      url: `https://alexcarpenter.me/${page.slug}`,
+      lastModified: page.updated || page.published,
+    }));
 
-  const routes = [""].map(
-    (route) => ({
-      url: `https://alexcarpenter.me${route}`,
-      lastModified: new Date().toISOString().split("T")[0],
-    })
-  );
+  const routes = [""].map((route) => ({
+    url: `https://alexcarpenter.me${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
   return [...routes, ...pages];
 }
