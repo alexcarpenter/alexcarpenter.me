0f559bf98b97fe6f18be15256187ab847a03493c
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-12-28 21:09:42 -0500

    prefers-color-scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
+  darkMode: "media",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
