46c04a6fb73c90068b5bb56abe05c2d2dd441254
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-12-28 20:43:28 -0500

    checkpoint at 2023-12-28T20:43:28-0500

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -5,33 +5,20 @@ import Link from "next/link";
 import { Badge } from "./badge";
 import { ButtonLink } from "./button-link";
 import { Columns } from "./columns";
-import { DescriptionList } from "./description-list";
 import { ImageCarousel } from "./image-carousel";
-import { MediaObject } from "./media-object";
 import { Note } from "./note";
 import { Spacer } from "./spacer";
 
 const components = {
+  // Custom components
   Badge,
   ButtonLink,
   Columns,
-  DescriptionList,
   Image,
   ImageCarousel,
-  MediaObject,
   Note,
   Spacer,
-  SearchLink: (props: { children: string }) => {
-    return (
-      <a
-        href={`https://duckduckgo.com/?q=${props.children}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {props.children}
-      </a>
-    );
-  },
+  // Navtive elements
   a: (props: any) => {
     if (props.href.startsWith("/")) {
       return <Link {...props} />;
