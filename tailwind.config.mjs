36fbfbe9b67264c9b5d9937bfb78af514b42df86
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-02-06 19:14:57 -0500

    rework font loading

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
+      sans: ["iA Writer Quattro", ...defaultTheme.fontFamily.sans],
       mono: [defaultTheme.fontFamily.mono],
     },
     extend: {
