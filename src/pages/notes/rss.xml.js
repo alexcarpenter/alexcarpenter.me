cc0236181a0d2dfdef8e5e0bbac5cc62360ab962
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-04-29 07:30:57 -0400

    checkpoint at 2024-04-29T07:30:57-0400

---
+++ src/pages/notes/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const notes = await getCollection("notes");
+  return rss({
+    title: "Notes // Alex Carpenter",
+    description: "Short-form thoughts and updates. ",
+    site: context.site,
+    items: notes.map((note) => ({
+      link: `/notes#${note.slug}`,
+      pubDate: note.data.published,
+      title: `Note: ${note.data.published}`,
+      content: sanitizeHtml(parser.render(note.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+    })),
+  });
+}
