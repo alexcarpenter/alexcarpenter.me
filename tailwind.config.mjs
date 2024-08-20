5d023bd079a9034d2449d11d3c01c102875bc6e5
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-20 08:24:23 -0400

    init

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,32 +9,9 @@ export default {
       sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
       mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
     },
-    fontSize: {
-      xs: "0.7142857143rem", // 10px
-      sm: "0.8571428571rem", // 12px
-    },
     extend: {
-      backgroundColor: {
-        primary: "var(--page-primary)",
-        surface: "var(--surface)",
-      },
-      textColor: {
-        primary: "var(--text-color-primary)",
-        secondary: "var(--text-color-secondary)",
-        subtle: "var(--text-color-subtle)",
-        accent: "var(--accent)",
-      },
-      borderColor: {
-        DEFAULT: "var(--border-color-primary)",
-        primary: "var(--border-color-primary)",
-        subtle: "var(--border-color-subtle)",
-      },
-      textDecorationColor: {
-        subtle: "var(--decoration-color-subtle)",
-        accent: "var(--accent)",
-      },
-      listStyleType: {
-        square: "square",
+      fontSize: {
+        xxs: "0.625rem",
       },
     },
   },
