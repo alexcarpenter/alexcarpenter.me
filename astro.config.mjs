e7e7927fc9d8fe50f8bd171224291b0aa53ca666
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-26 08:45:51 -0400

    checkpoint at 2024-08-26T08:45:51-0400

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,29 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 // Remark plugins
-import {
-  remarkDefinitionList,
-  defListHastHandlers,
-} from "remark-definition-list";
+import { remarkDefinitionList, defListHastHandlers } from "remark-definition-list";
 import remarkAdmonitions from "remark-github-beta-blockquote-admonitions";
-import {
-  transformerMetaHighlight,
-  transformerMetaWordHighlight,
-} from "@shikijs/transformers";
-
+import { transformerMetaHighlight, transformerMetaWordHighlight } from "@shikijs/transformers";
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexcarpenter.me",
-  integrations: [
-    sitemap(),
-    tailwind({
-      nesting: true,
-    }),
-  ],
+  integrations: [sitemap(), tailwind({
+    nesting: true
+  }), icon()],
   markdown: {
     remarkPlugins: [remarkDefinitionList, remarkAdmonitions],
     remarkRehype: {
       handlers: {
-        ...defListHastHandlers,
-      },
+        ...defListHastHandlers
+      }
     },
     shikiConfig: {
       theme: "css-variables",
-      transformers: [
-        transformerMetaHighlight(),
-        transformerMetaWordHighlight(),
-      ],
-    },
-  },
+      transformers: [transformerMetaHighlight(), transformerMetaWordHighlight()]
+    }
+  }
 });
