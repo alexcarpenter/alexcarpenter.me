8a7ea1dc4eabb404452aedeb46f1ff9917464037
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-09-22 08:56:13 -0400

    checkpoint at 2024-09-22T08:56:13-0400

--- src/utils/embla.ts
+++ src/utils/embla.ts
@@ -50,3 +50,46 @@ export const addPrevNextBtnsClickHandlers = (
     nextBtn.removeEventListener("click", scrollNext, false);
   };
 };
+
+export const addDotBtnsAndClickHandlers = (
+  emblaApi: EmblaCarouselType,
+  dotsNode: HTMLElement,
+): (() => void) => {
+  let dotNodes: HTMLElement[] = [];
+
+  const addDotBtnsWithClickHandlers = (): void => {
+    dotsNode.innerHTML = emblaApi
+      .scrollSnapList()
+      .map(() => '<button class="embla__dot" type="button"></button>')
+      .join("");
+
+    const scrollTo = (index: number): void => {
+      emblaApi.scrollTo(index);
+    };
+
+    dotNodes = Array.from(dotsNode.querySelectorAll(".embla__dot"));
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener("click", () => scrollTo(index), false);
+    });
+  };
+
+  const toggleDotBtnsActive = (): void => {
+    const previous = emblaApi.previousScrollSnap();
+    const selected = emblaApi.selectedScrollSnap();
+    dotNodes[previous].dataset.active = "false";
+    dotNodes[selected].classList.remove("active");
+    dotNodes[selected].dataset.active = "true";
+    dotNodes[selected].classList.add("active");
+  };
+
+  emblaApi
+    .on("init", addDotBtnsWithClickHandlers)
+    .on("reInit", addDotBtnsWithClickHandlers)
+    .on("init", toggleDotBtnsActive)
+    .on("reInit", toggleDotBtnsActive)
+    .on("select", toggleDotBtnsActive);
+
+  return (): void => {
+    dotsNode.innerHTML = "";
+  };
+};
