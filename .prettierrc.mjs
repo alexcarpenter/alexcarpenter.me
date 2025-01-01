0ab986ae234ae6c8485abd6eb29780bb8128bbb9
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-01-01 13:34:47 -0500

    checkpoint at 2025-01-01T13:34:47-0500

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
   overrides: [
     {
       files: "*.astro",
