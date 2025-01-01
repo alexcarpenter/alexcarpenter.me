f15e0d185b63a6bd64ed16f97f354c132a53e1d2
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-01-01 10:30:40 -0500

    checkpoint at 2025-01-01T10:30:40-0500

---
+++ src/pages/links/rss.xml.js
@@ -0,0 +1,25 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const links = await getCollection("links");
+
+  return rss({
+    title: "Links // Alex Carpenter",
+    description: "",
+    site: context.site,
+    items: links.map((item) => {
+      return {
+        title: item.data.title,
+        pubDate: item.data.published,
+        link: item.data.link,
+        content: sanitizeHtml(parser.render(item.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
+      };
+    }),
+  });
+}
