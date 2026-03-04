24b1f6f4a85fc8839046c3dd6cf3f5d9804b89bc
Alex Carpenter <im.alexcarpenter@gmail.com>
2026-03-04 08:45:29 -0500

    format link hostname to include x username

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ export function enforceExhaustive(
 ): never {
   throw new Error(`${message} '${value}'`);
 }
+
+export function formatLinkHostname(urlString: string): string {
+  const url = new URL(urlString);
+
+  // For Twitter/X links, show x.com/username or twitter.com/username
+  if (url.hostname.includes("twitter.com") || url.hostname.includes("x.com")) {
+    const username = url.pathname.split("/")[1];
+    return username ? `${url.hostname}/${username}` : url.hostname;
+  }
+
+  // For all other links, just show the hostname
+  return url.hostname;
+}
